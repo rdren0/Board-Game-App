772cc0884f1b46eb169aae6213e058c8b13f5dfb
Erik Konner <erikkonner@gmail.com>
2019-04-03 14:33:08 -0600

    Header background image
    Co-authored-by: Rachael <rdrennan0@gmail.com>
    Co-authored-by: Mark <markcollins.md@gmail.com>
    Co-authored-by: Colby <colbyallen012@gmail.com>

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/Header.css'
+
+function Header() {
+  return (
+    <header>
+      <h1>Board, not bored</h1>
+    </header>
+  )
+}
+
+export default Header;
