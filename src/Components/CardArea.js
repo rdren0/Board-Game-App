b4f745bd2b46baea52bd33ff5d3c932d4c19d8ac
Rachael Drennan <rdrennan0@gmail.com>
2019-04-05 12:00:06 -0600

    add location cards
    Co-authored-by: Mark <markcollins.md@gmail.com>
    Co-authored-by: Colby <colbyallen012@gmail.com>
    Co-authored-by: Erik <erikkonner@gmail.com>

--- src/Components/CardArea.js
+++ src/Components/CardArea.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import App from '../App.js';
 import NavBar from './NavBar.js'
 import Game from './Game.js'
+import Location from './Location.js'
 import '../css/CardArea.css'
 
 class CardArea extends Component {
@@ -13,26 +14,42 @@ class CardArea extends Component {
   }
 
   render() {
-    if (this.props.gamesData) {
-    let gamesCards =
-     this.props.gamesData.map((game) => {
-      return <Game
-      name={game.name}
-      minPlayers={game.minPlayers}
-      maxPlayers={game.maxPlayers}
-      weight={game.weight}
-      timeToPlay={game.timeToPlay}
-      type={game.type}
-      features={game.features}
-      image={game.image}
-      id={game.gameId}
-      />
+    if (this.props.locationData) {
+    // let gamesCards =
+    //  this.props.gamesData.map((game) => {
+    //   return <Game
+    //   name={game.name}
+    //   minPlayers={game.minPlayers}
+    //   maxPlayers={game.maxPlayers}
+    //   weight={game.weight}
+    //   timeToPlay={game.timeToPlay}
+    //   type={game.type}
+    //   features={game.features}
+    //   image={game.image}
+    //   id={game.gameId}
+    //   />
+  let locationCards =
+     this.props.locationData.map((location) => {
+      return <Location
+      name={location.name}
+      address = {location.address}
+      website = {location.website}
+      hasFood = {location.hasFood}
+      hasDrinks = {location.hasDrinks}
+      sellsGames = {location.sellsGames}
+      weekdayOpen = {location.weekdayOpen}
+      weekdayClose = {location.weekdayClose}
+      weekendOpen = {location.weekendOpen}
+      weekendClose = {location.weekendClose}
+      googleMapsLink = {location.googleMapsLink}
+      bringYourOwnGame = {location.bringYourOwnGame}
+      gamesOffered = {location.gamesOffered}/>
     });
     return (
       <div>
         <NavBar NavBar={NavBar}/>
         <section className = 'cardContainer'>
-          {gamesCards}
+          {locationCards}
         </section>
       </div>
     )
