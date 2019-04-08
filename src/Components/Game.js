c2a45595681c17f7e8d6b553b0fdb9500ec86805
Colby Allen <colbyallen012@gmail.com>
2019-04-08 08:49:01 -0600

    Add svg color and Favorite button

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -11,6 +11,7 @@ function Game(props) {
           <p className="text">Players: {props.minPlayers}-{props.maxPlayers}</p>
           <p className="text">Weight: {props.weight}</p>
           <p className="text">Type: {props.type}</p>
+          <p className="text">Favorite</p>
         </div>
     </div>
   )
