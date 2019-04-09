304e381b7589a0fd38cc990c94530d3fc6796b08
Erik Konner <erikkonner@gmail.com>
2019-04-08 21:49:45 -0600

    Added method to splice index of name out of array

--- src/Components/LikeButton.js
+++ src/Components/LikeButton.js
@@ -5,7 +5,7 @@ class LikeButton extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isItemLiked: true
+      isItemLiked: false
     }
     this.favoritesStorage = this.props.favorites
     console.log("like button",this.favoritesStorage);
@@ -19,7 +19,7 @@ class LikeButton extends Component {
     const articleEl = e.target.closest('article');
     const title = articleEl.getAttribute('value');
     this.favoritesStorage.push(title);
-    this.saveToStorage();
+    this.deleteFromStorage(title);
   }
 
   saveToStorage() {
@@ -27,11 +27,16 @@ class LikeButton extends Component {
     console.log("save to Storage method", this.favoritesStorage);
   }
 
+  deleteFromStorage(title) {
+    const index = this.favoritesStorage.indexOf(title);
+    this.state.isItemLiked ? this.favoritesStorage.splice(index, 1) : this.saveToStorage()
+    }
+
   render() {
     return(
       <div>
         <img onClick={this.handleLike}
-        src={this.state.isItemLiked ? this.props.unlike : this.props.like}
+        src={this.state.isItemLiked ? this.props.like : this.props.unlike}
         value={this.state.isItemLiked ? this.props.unlike : this.props.like}
         className="like-btn"></img>
       </div>
